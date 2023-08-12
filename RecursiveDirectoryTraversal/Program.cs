7d3fd5a120bf4fee35d59e68e7cc30e913ff6346
Admin <Admin@DESKTOP-047GBOQ>
2023-08-12 13:50:28 +0300

    Добавьте файлы проекта.

---
+++ RecursiveDirectoryTraversal/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите путь к каталогу: ");
+string path = Console.ReadLine();
+
+TraverseDirectory(path);
+
+Console.WriteLine("Обход завершен.");
+
+static void TraverseDirectory(string directory)
+{
+    try
+    {
+        string[] files = Directory.GetFiles(directory);
+        string[] subdirectories = Directory.GetDirectories(directory);
+
+        Console.WriteLine($"Файлы в каталоге {directory}:");
+        foreach (string file in files)
+        {
+            Console.WriteLine(file);
+        }
+
+        Console.WriteLine($"Подкаталоги в каталоге {directory}:");
+        foreach (string subdirectory in subdirectories)
+        {
+            Console.WriteLine(subdirectory);
+            TraverseDirectory(subdirectory);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ошибка при обходе каталога {directory}: {ex.Message}");
+    }
+}
